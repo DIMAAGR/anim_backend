514184367abdd66d99c23c9c01aafb023288e9d3
Italo Matos <attackdiamond@gmail.com>
2023-04-12 17:55:47 -0300

    adicionado sessionDurationTimeInSeconds

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Este projeto utiliza principalmente o shelf com o dart e o hive como DB NoSQL.
 ------------------------------------------------------------------------------
 ### Ordem de criação do backend
 
-1 - autenticação (registro, login, esqueci a senha).
+1 - autenticação login.
+2 - sistema de erros.
+3 - sistema de sessões com UUID.
--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,6 +2,7 @@ class Const {
   static const String serverIP = 'localhost';
   static const int port = 8080;
 
+  static const int sessionDurationTimeInSeconds = 28800;
 
-  static const Map<String, Object> jsonContentType = {'content-type' : 'application/json'};
+  static const Map<String, Object> jsonContentType = {'content-type': 'application/json'};
 }
